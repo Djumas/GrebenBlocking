fb53fff81a6d40de23937e50a5af0a617f671e35
Святослав Пальчунов <sdpa89@gmail.com>
2021-11-21 10:27:05 +0700

    Added Behavior designer plug-in. Made FoolAI first steps.

---
+++ Assets/FoolIdle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoolIdle : ScriptedBehavior
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    override public void Run() {
+        Debug.Log("idling");
+    }
+}
---
+++ Assets/ScriptedBehaviorRunner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptedBehaviorRunner : MonoBehaviour
+{
+    public ScriptedBehavior currentBehavior;
+    [System.Serializable]
+    public struct NamedBehavior {
+        public string name;
+        public ScriptedBehavior behavior;
+    }
+    public NamedBehavior[] behaviors;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AssignBehavior(string behavior) {
+        foreach (NamedBehavior namedBehavior in behaviors) {
+            if (namedBehavior.name == behavior) {
+                currentBehavior = namedBehavior.behavior;
+                return;
+            }
+        }
+        Debug.Log("No such behavior");
+    }
+}
